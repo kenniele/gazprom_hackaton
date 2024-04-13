c558264937991818628c7d8da52555cdb04176bc
kenniele <kenniele@yandex.com>
2024-04-13 15:19:15 +0300

    version 0.6.2 upd normal version  (1, 0.5, 1) btw almost 100% accuracy

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ async def main():
     bot = Bot(token=bot_token, parse_mode=None)
     logging.basicConfig(level=logging.INFO)
 
-    @dp.message(Command(commands=["start"]))
+    @dp.channel_post(Command(commands=["start"]))
     async def command_start(message):
         global consult_agent
         consult_agent = ConsultGPT.from_llm(llm, verbose=False)
         consult_agent.seed_agent()
         ai_message = consult_agent.ai_step()
-        await message.answer(ai_message)
+        # await message.answer(ai_message)
 
-    @dp.message(F.text)
+    @dp.channel_post(F.text)
     async def process_text(message):
         if consult_agent is None:
             await message.answer("Используйте команду /start")
@@ -39,12 +39,12 @@ async def main():
             ai_message = consult_agent.ai_step()
             await message.answer(ai_message)
 
-    @dp.message(~F.text)
+    @dp.channel_post(~F.text)
     async def process_other(message):
         await message.answer("Бот принимает только текстовые сообщения")
 
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, allowed_updates=["channel_post"])
 
 
 if __name__ == "__main__":
